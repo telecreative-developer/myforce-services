7e40c159024a9c128e489f0c673b9ad0cf69c809
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-02-15 13:07:55 +0700

    Fix field PIC

--- src/models/pipeline-products.model.js
+++ src/models/pipeline-products.model.js
@@ -3,30 +3,36 @@
 const Sequelize = require('sequelize');
 const DataTypes = Sequelize.DataTypes;
 
-module.exports = function (app) {
+module.exports = function(app) {
   const sequelizeClient = app.get('sequelizeClient');
-  const pipelineProducts = sequelizeClient.define('pipeline_products', {
-    id_pipeline_product: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+  const pipelineProducts = sequelizeClient.define(
+    'pipeline_products',
+    {
+      id_pipeline_product: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      id_pipeline: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      id_product: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
     },
-    id_pipeline: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    id_product: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
-  }, {
-    hooks: {
-      beforeCount(options) {
-        options.raw = true;
+    {
+      hooks: {
+        beforeCount(options) {
+          options.raw = true;
+        }
       }
     }
-  });
+  );
 
-  pipelineProducts.associate = function (models) { // eslint-disable-line no-unused-vars
+  pipelineProducts.associate = function(models) {
+    // eslint-disable-line no-unused-vars
     // Define associations here
     // See http://docs.sequelizejs.com/en/latest/docs/associations/
   };

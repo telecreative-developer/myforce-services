03dabcce9fca4cf01b24e9c8c89a7e2a11d0f95d
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-05-16 22:56:42 +0700

    Fix primary key on events

--- src/services/events/events.service.js
+++ src/services/events/events.service.js
@@ -1,23 +1,24 @@
 // Initializes the `events` service on path `/events`
-const createService = require('feathers-sequelize');
-const createModel = require('../../models/events.model');
-const hooks = require('./events.hooks');
+const createService = require('feathers-sequelize')
+const createModel = require('../../models/events.model')
+const hooks = require('./events.hooks')
 
-module.exports = function (app) {
-  const Model = createModel(app);
-  const paginate = app.get('paginate');
+module.exports = function(app) {
+  const Model = createModel(app)
+  const paginate = app.get('paginate')
 
   const options = {
     name: 'events',
+    id: 'id_event',
     Model,
     paginate
-  };
+  }
 
   // Initialize our service with any options it requires
-  app.use('/events', createService(options));
+  app.use('/events', createService(options))
 
   // Get our initialized service so that we can register hooks and filters
-  const service = app.service('events');
+  const service = app.service('events')
 
-  service.hooks(hooks);
-};
+  service.hooks(hooks)
+}
